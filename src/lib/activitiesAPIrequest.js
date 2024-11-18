1ed9d64f6a57f69f3dd6e3a03032d585aee84448
Soohyeun <soohyeunp@gmail.com>
2024-11-18 01:48:46 -0800

    Fetch all activities via API

--- src/lib/activitiesAPIrequest.js
+++ src/lib/activitiesAPIrequest.js
@@ -1,4 +1,28 @@
 /**
+ * Fetch all activities.
+ * @returns all activities as a list
+ */
+export const fetchAllActivity = async () => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/activities`);
+    if (response.status === 404) {
+      return { error: 'Acticity not found (404)', status: 404 };
+    }
+
+    if (!response.ok) {
+      return {
+        error: `Failed to fetch an activity (status: ${response.status})`,
+      };
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    return { error: 'Failed to fetch an activity', details: error.message };
+  }
+};
+
+
+/**
  * Fetch a specific activity by an id.
  * @param {number} callId - id of a activity
  * @returns activity as an object
